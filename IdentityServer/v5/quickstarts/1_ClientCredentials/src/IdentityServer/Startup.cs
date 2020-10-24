ffb3af5daba4c16343fd562bd6aa57e6c4a34d6e
Dominick Baier <dbaier@leastprivilege.com>
2020-10-24 11:12:50 +0200

    update qs1

--- IdentityServer/v5/quickstarts/1_ClientCredentials/src/IdentityServer/Startup.cs
+++ IdentityServer/v5/quickstarts/1_ClientCredentials/src/IdentityServer/Startup.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
 
 
 using Microsoft.AspNetCore.Builder;
@@ -14,11 +14,9 @@ namespace IdentityServer
             // uncomment, if you want to add an MVC-based UI
             //services.AddControllersWithViews();
 
-            var builder = services.AddIdentityServer()
+            services.AddIdentityServer()
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients);
-
-            builder.AddDeveloperSigningCredential();
         }
 
         public void Configure(IApplicationBuilder app)
