6de1c72fb641fc61276a971f37d9702b0481adff
khalidabuhakmeh <khalidabuhakmeh@gmail.com>
2025-01-31 14:14:20 -0500

    Disable v5 documentation build in build.sh
    Commented out the commands for building IdentityServer v5 docs. This ensures only v6 and v7 documentation is built during the process.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 set -e
 
-pushd ./IdentityServer/v5/docs
-hugo -d ../../../root/identityserver/v5
-popd
+#pushd ./IdentityServer/v5/docs
+#hugo -d ../../../root/identityserver/v5
+#popd
 
 pushd ./IdentityServer/v6/docs
 hugo -d ../../../root/identityserver/v6
