62509ccdd282d99df6f0b3c10f4d0ab9c4b2e078
khalidabuhakmeh <khalidabuhakmeh@gmail.com>
2025-03-27 11:55:49 -0400

    tighten up imports

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,8 @@
-import {defineCollection} from 'astro:content';
+import { defineCollection, z } from 'astro:content'
 import {docsLoader} from '@astrojs/starlight/loaders';
 import {docsSchema} from '@astrojs/starlight/schema';
 import {autoSidebarLoader} from 'starlight-auto-sidebar/loader';
 import {autoSidebarSchema} from 'starlight-auto-sidebar/schema';
-import { z } from 'astro/zod'
 
 export const collections = {
 	docs: defineCollection({
